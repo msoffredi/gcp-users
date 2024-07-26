9d239dde558e767ad285fc212651352080b438a7
Marcelo Soffredi <msoffredi@gmail.com>
2024-07-25 19:21:45 -0500

    Updated test setup to install MongoDB in memory

--- __tests__/utils/setup.ts
+++ __tests__/utils/setup.ts
@@ -1,3 +1,6 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+
 // jest.mock('@advocate-insights/ms-common', () => {
 //     const originalModule = jest.requireActual('@advocate-insights/ms-common');
 
@@ -9,18 +12,36 @@
 // });
 
 // Environment variables
-// process.env.USER_TABLE_NAME = 'user';
-// process.env.CLIENT_TABLE_NAME = 'user-client';
-// process.env.EVENT_BUS_NAME = 'test';
-// process.env.REACT_APP_COGNITO_CLIENT_ID = 'abc123';
-// process.env.USER_POOL_NAME = 'abc123';
-// process.env.AWS_REGION = 'us-east-1';
+process.env.DB_USER = 'user';
+process.env.DB_PASSWORD = 'password';
+process.env.DB_HOST = 'host';
+process.env.DB_NAME = 'database_name';
 
 if (!process.env.DEBUG) {
     global.console.log = jest.fn();
     global.console.error = jest.fn();
 }
 
-beforeAll(async () => {});
+let mongo: MongoMemoryServer;
 
-beforeEach(async () => {});
+beforeAll(async () => {
+    mongo = await MongoMemoryServer.create();
+    const mongoUri = mongo.getUri();
+
+    await mongoose.connect(mongoUri, {});
+});
+
+afterAll(async () => {
+    if (mongo) {
+        await mongo.stop();
+    }
+    await mongoose.connection.close();
+});
+
+beforeEach(async () => {
+    const collections = await mongoose.connection.db.collections();
+
+    for (let collection of collections) {
+        await collection.deleteMany({});
+    }
+});
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    // preset: '@shelf/jest-dynamodb',
     modulePathIgnorePatterns: [
         '<rootDir>/__tests__/utils',
         '<rootDir>/__tests__/events',
@@ -10,8 +9,6 @@ module.exports = {
     setupFilesAfterEnv: ['./__tests__/utils/setup.ts'],
     collectCoverageFrom: ['src/**/*.{ts,js}'],
     transform: {
-        // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
-        // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
         '^.+\\.tsx?$': ['ts-jest'],
     },
 };
